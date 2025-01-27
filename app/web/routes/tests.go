b9d7c732db47788c9d0b65578b12760cc1a109d7
Matt Townsend <mt76@gmx.com>
2025-01-27 16:16:53 +0000

    Update TextStore structure and add consumer tracking; enhance .gitignore for backup, database, and log files

--- app/web/routes/tests.go
+++ app/web/routes/tests.go
@@ -5,7 +5,6 @@ import (
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
-	"github.com/mt1976/trnsl8r_service/app/dao/textStore"
 	"github.com/mt1976/trnsl8r_service/app/support/logger"
 	"github.com/mt1976/trnsl8r_service/app/support/paths"
 	"github.com/mt1976/trnsl8r_service/app/web/pages"
@@ -25,19 +24,4 @@ func Test(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	if err != nil {
 		logger.ErrorLogger.Print(err.Error())
 	}
-}
-
-func ExportTranslations(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	//logger.EventLogger.Printf("[TEST] [View]")
-
-	trace(r)
-
-	err := textStore.ExportCSV()
-	if err != nil {
-		logger.ErrorLogger.Print(err.Error())
-		oops(w, r, nil, "error", err.Error())
-	}
-	//successMessage(w, r, nil, "success - translations exported")
-	logger.EventLogger.Printf("[TEST] [Export] [Translations] [Success]")
-	http.Redirect(w, r, "/test", http.StatusSeeOther)
 }
